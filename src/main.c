c2e9d2104ea955e4f3985abb821cf0be00135242
Ahmed Said <said.ahmed3617@gmail.com>
2024-08-12 21:38:45 +0300

    UART: Sending and transmitting directly from registers.

--- src/main.c
+++ src/main.c
@@ -3,23 +3,38 @@
 #include "systick.h"
 #include "gpio.h"
 
-
 int main()
 {
     RCC_USART2_ENABLE();
+    RCC_PORTA_ENABLE();
 
-    USART2->CR1 |= (1<<13);
+    // PA8 OUT
+    GPIO_Init(GPIOA, 8, GPIO_MODE_OUTPUT_PP, GPIO_PULL_FLOATING);
+
+    // PA2 (TX) AFIO Pull Push
+    GPIOA->CRL &= 0xF << (2 * 4);
+    GPIOA->CRL |= 0x9 << (2 * 4);
+
+    // PA3 (RX) GPIO Input floating
+    GPIO_Init(GPIOA, 3, GPIO_MODE_INPUT, GPIO_PULL_FLOATING);
+
+    USART2->CR1 |= (1 << 13);
     USART2->BRR |= 52 << 4;
     USART2->BRR |= 1;
-    USART2->CR1 |= (1<<3);
-
-    // PA2 TX
+    USART2->CR1 |= (1 << 2);
 
     while (1)
     {
-        USART2->DR = 'A';
-        SysTick_delay_ms(500);
-        
+        if (USART2->SR & (1<<5))
+        {
+            if (USART2->DR == 'A')
+            {
+                GPIO_write_pin(GPIOA, 8, 1);
+                SysTick_delay_ms(100);
+                GPIO_write_pin(GPIOA, 8, 0);
+            }
+        }
+
     }
 
     return 0;
