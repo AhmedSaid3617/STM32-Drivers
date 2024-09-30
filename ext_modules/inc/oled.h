dbee874b91a888d9e28c1ee3ef31df87c4c024ab
Ahmed Said <said.ahmed3617@gmail.com>
2024-09-30 12:48:55 +0300

    OLED: Added oled draw fucntion, not working properly.

--- ext_modules/inc/oled.h
+++ ext_modules/inc/oled.h
@@ -3,6 +3,12 @@
 
 #include "i2c.h"
 
-void OLED_init(I2C_TypeDef* I2C_base);
+typedef struct
+{
+    uint8_t data_byte;
+    uint8_t pixels[8][128];
+} frame_t;
 
+void OLED_init(I2C_TypeDef* I2C_base, frame_t* frame);
+void OLED_draw_rectangle(frame_t* frame, uint8_t x, uint8_t y, uint8_t length, uint8_t width);
 #endif
--- ext_modules/src/oled.c
+++ ext_modules/src/oled.c
@@ -1,9 +1,50 @@
 #include "oled.h"
 
-void OLED_init(I2C_TypeDef* I2C_base){
+/**
+ * @brief Initialize the OLED display.
+ * 
+ * @param I2C_base I2C module connected to the OLED.
+ * @param frame The frame_t object that will be written to the OLED display.
+ */
+void OLED_init(I2C_TypeDef* I2C_base, frame_t* frame){
     uint8_t commands[] = {0x00, 0xAE, 0xD5, 0xF0, 0xA8, 64 - 1, 0x8D, 0x14, 0xAF, 0xA4, 0x20, 0x00};
     uint8_t start[] = {0x00, 0x21, 0, 127, 0x22, 0, 7};
 
+    // Initialize frame.
+    frame->data_byte = 0x40;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 128; j++)
+        {
+            frame->pixels[i][j] = 0;
+        }
+    }
+
     I2C_master_send(I2C_base, 0x3C, &commands, 12);
     I2C_master_send(I2C_base, 0x3C, &start, 7);
+}
+
+
+void OLED_draw_rectangle(frame_t* frame, uint8_t x, uint8_t y, uint8_t length, uint8_t width){
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 128; j++)
+        {
+            frame->pixels[i][j] = 0;
+        }
+    }
+
+    for (int i = x; (i < x+width && i < 128); i++)
+    {
+        frame->pixels[y/8][i] = 0xff << y%8;
+        
+        if (length <= 8 - y%8)
+        {
+            frame->pixels[y/8][i] &= 0xff >> (8 - y%8 - length);
+        }
+
+        
+        
+    }
+    
 }
